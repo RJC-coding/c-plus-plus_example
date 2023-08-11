7ff48e0ad4e1bd55aec37562101ff02142dc5335
Robert Camp s3909817 <Robert.James.Camp@gmail.com>
2023-08-11 13:39:58 +1000

    Letter counter

---
+++ letterCounter/letterCounter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cctype>
+
+int main()
+{
+    char word[100];
+    std::cin.getline(word, sizeof(word));
+
+    int counter = 0;
+
+    for (int i = 0; i < sizeof(word); i++) {
+        char c = tolower(word[i]);
+        int ascii = int(c);
+        if (ascii >= 97 & ascii <= 122){
+            counter++;
+        }
+    }
+    std::cout << "This phrase has " << counter << " letters. \n";
+    return 0;
+}

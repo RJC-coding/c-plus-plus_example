d89f9fa01016c8438b6f4ca58df30ab804831f1f
Robert Camp s3909817 <Robert.James.Camp@gmail.com>
2023-08-15 13:12:48 +1000

    Start search

--- fileRead/Main.cpp
+++ fileRead/Main.cpp
@@ -2,16 +2,22 @@
 #include <fstream>;
 #include <string>;
 
+
 //https://cplusplus.com/doc/tutorial/files/
 //https://www.geeksforgeeks.org/getline-string-c/
+//https://www.digitalocean.com/community/tutorials/convert-string-to-char-array-c-plus-plus
 
 int main() {
 	std::string line;
 	std::fstream file;
+	std:char charList[] = "";
 	file.open("example.txt");
 	while (getline(file, line))
 	{
-		std::cout << line << "\n";
+		for (int c = 0; c < line.length(); c++) {
+			std::cout << line[c];
+		}
+		std::cout << "\n";
 	}
 	file.close();
 	return 0;
